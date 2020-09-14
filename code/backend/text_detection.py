7afb3869596e1120a6d353f50879569c1b95e14b
김낙현 <sonata0226@gmail.com>
2020-09-14 15:47:30 +0900

    S03P22B105-48 S03P22B105-49 / Add make detection func
    Former-commit-id: 0c4b0617adbd6ce1706273b95eae8050cff3ddce

--- code/backend/text_detection.py
+++ code/backend/text_detection.py
@@ -13,21 +13,27 @@ import os # os의 기능을 사용하기 위한 모듈
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=".\\datasets\\My Project-070d5c77d071.json"
-client = vision.ImageAnnotatorClient()
+def detection(image):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=".\\datasets\\My Project-070d5c77d071.json"
+    client = vision.ImageAnnotatorClient()
 
-# 이미지 읽기
-with io.open('.\\datasets\\kakao.png', 'rb') as image_file:
-    content = image_file.read()
+    # 이미지 읽기
+    with io.open(image, 'rb') as image_file:
+        content = image_file.read()
 
-image = vision.types.Image(content=content)
+    image = vision.types.Image(content=content)
 
-response = client.text_detection(image=image)
-texts = response.text_annotations
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
 
-# print(texts)
-for text in texts:
-        print('\n"{}"'.format(text.description))
+    list = []
+
+    # print(texts)
+    for text in texts:
+        # print('\n"{}"'.format(text.description))
         vertices = (['({},{})'.format(vertex.x, vertex.y)
                     for vertex in text.bounding_poly.vertices])
-        print('bounds: {}'.format(','.join(vertices)))
+        # print('bounds: {}'.format(','.join(vertices)))
+        list.append(text.description)
+    list.remove(list[0])
+    return list
