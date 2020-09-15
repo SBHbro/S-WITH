cdab1d243032b0bcc722abfb7eed4de0e1954e8d
안성호 <jugiaro5@naver.com>
2020-09-15 14:48:14 +0900

    S03P22B105-62 / Add Object Detection and Delete ods folder
    Former-commit-id: ba18c9fc291cc10ca9684ba03f9d29201e8c7a34

--- code/Django/project/project/ai/views.py
+++ code/Django/project/project/ai/views.py
@@ -11,13 +11,17 @@ from rest_framework.response import Response  # 응답하는 메서드
 from rest_framework.decorators import api_view  # 요청 방식을 필터링
 import os
 
-from .func import text_detection
+
+from .func import text_detection, google_object
 
 @api_view(['GET'])
-def imageUpload(request):
-    print("=============================================")
+def textDetection(request):
     address = os.path.dirname(os.path.abspath(__file__)) + '\\func\\datasets\\kakao.png'
 
-    text_detection.detection(address)
-    return Response({'message': '성공적으로 삭제되었습니다.'})
+    list = text_detection.detection(address)
+    return Response(list)
 
+@api_view(['GET'])
+def objectDetection(request):
+    list = google_object.run()
+    return Response(list)
