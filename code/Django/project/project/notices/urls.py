9495c7f40085af37e53af6f5e628840a92217652
jugiaro5 <jugiaro5@naver.com>
2020-09-17 14:11:48 +0900

    None / Update Django CRUD
    Former-commit-id: 1d5f7c11e6c56484cba2cfee26104a4763981573

--- code/Django/project/project/notices/urls.py
+++ code/Django/project/project/notices/urls.py
@@ -6,9 +6,6 @@ from . import views
 app_name = 'notices'
 
 urlpatterns =[
-    path('notice/', views.notice_select, name='notice_select'),
-    path('notice/<int:notice_pk>/', views.notice_detail, name='notice_detail'),
-    path('notice/create/', views.notice_create, name='notice_create'),
-    path('notice/update/<int:notice_pk>/', views.notice_update, name='notice_update'),
-    path('notice/delete/<int:notice_pk>/', views.notice_delete, name='notice_delete'),
+    path('notice', views.notice_list, name='notice_list'),
+    path('notice/<int:notice_pk>', views.notice_detail, name='notice_detail'),
 ]
