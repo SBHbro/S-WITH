e13119af3997d1d302152d881069cefe68116696
김낙현 <sonata0226@gmail.com>
2020-09-21 14:15:08 +0900

    delete
    Former-commit-id: ccaea3ee5fff085e95c62b1ab2d66b2898df3cf3

--- code/Django/crud/articles/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Article
-
-# 내부적으로 검증하는 프로그램?
-class ArticleForm(forms.ModelForm):
-    class Meta:
-        model = Article
-        fields = ['title', 'content']
--- code/Django/project/project/notices/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Notice(models.Model):
-    # 모델의 기본키 필드는 별도로 지정하지 않으면 자동으로 추가된다.
-    subject = models.CharField(max_length=100)
-    content = models.TextField()
-    email = models.CharField(max_length=50)
-    date = models.DateTimeField(auto_now_add=True)
