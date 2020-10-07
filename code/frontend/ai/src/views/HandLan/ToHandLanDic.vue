2bd6e8de87d7bea5b86aef58a47046a8348f8ea3
류혜명 <bu031015080@gmail.com>
2020-10-07 17:44:02 +0900

    final
    Former-commit-id: eef59399ac4b9a33df49bc4d3c1babe9833da5da [formerly d9b23112f79e742ade039d7125e14a525ada84f3]
    Former-commit-id: 760184aa1f0e7b96d95b95743e272719e709fa8a

--- code/frontend/ai/src/views/HandLan/ToHandLanDic.vue
+++ code/frontend/ai/src/views/HandLan/ToHandLanDic.vue
@@ -14,7 +14,7 @@
       <div id="search" :style="{'margin-left':searchMargin+'px'}" style="width: 100%; 
 height: 50px; 
 max-width:550px; 
-background-color: rgb(243, 242, 242); border-radius: 45px; 
+background-color:white; border-radius: 45px; 
 padding:10px 10px 10px 30px;">
         <v-icon size="30" style="float:left;margin-right:20px;">mdi-magnify</v-icon>
         <input v-on:keyup.enter="transButton" v-model="attr" placeholder="수어로 번역할 한글을 입력해주세요." type="text" style="height:100%; overflow:hidden;float:left;" :style="{width:'50%'}">
